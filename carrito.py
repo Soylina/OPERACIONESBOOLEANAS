21b83c0473dd63a269aafe61fe43aabd44320a9e
Lina Briñez <111194385+Soylina@users.noreply.github.com>
2022-09-12 22:29:33 -0500

    Update carrito.py

--- carrito.py
+++ carrito.py
@@ -1,23 +1,23 @@
 from machine import Pin as pin 
 from utime import sleep
 
-ledrojo = pin(33,pin.OUT)
-puerta1 = pin(4,pin.IN)
-puerta2 = pin(16,pin.IN)
-puerta3= pin(23,pin.IN)
-puerta4 = pin(19,pin.IN)
+led = pin(25,pin.OUT)
+puerta1 = pin(15,pin.IN)
+puerta2 = pin(4,pin.IN)
+puerta3= pin(17,pin.IN)
+puerta4 = pin(18,pin.IN)
 pausa = 0.1
 def prende():
   print("prende")
-  ledrojo.value(1)
+  led.value(1)
   sleep(pausa)
-  ledrojo.value(0)
+  led.value(0)
 def apaga():
   print("apaga")
-  ledrojo.value(0)
-  
+  led.value(0)
+   
 while (True):
-   if not(puerta1.value() and puerta2.value()):
+   if not(puerta1.value() and puerta2.value()and puerta3.value() and puerta4.value()):
     prende() 
 
     
